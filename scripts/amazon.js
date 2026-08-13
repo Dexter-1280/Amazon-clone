7ef6594a235549b1b2f6853542afc029aae01a77
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-08-13 20:54:44 +0530

    amazon clone

---
+++ scripts/amazon.js
@@ -0,0 +1,97 @@
+import {cart , addToCart, calculateCartQuantity} from '../data/cart.js'
+import { products } from '../data/products.js'
+import { fixCurrency } from './utils/money.js'
+
+
+let productsHTML = ''
+products.forEach((product) => {
+  productsHTML += `
+    <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src=${product.image}>
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars *10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${fixCurrency(product.priceCents)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select class="js-quantity-selector-${product.id}">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart js-added-to-cart-${product.id}">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id = '${product.id}'>
+            Add to Cart
+          </button>
+        </div>
+  `
+})
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML
+
+
+function updateCartQuantity() {
+  document.querySelector('.js-cart-quantity').innerHTML = calculateCartQuantity()
+}
+
+function addAddedElement(productId) {
+  let addedTimeoutId;
+  const addedElement = document.querySelector(`.js-added-to-cart-${productId}`)
+  addedElement.classList.add('added-to-cart-after')
+  clearTimeout(addedTimeoutId)
+  addedTimeoutId = setTimeout(() => {
+    addedElement.classList.remove('added-to-cart-after')
+  },2000)
+}
+
+
+document.querySelectorAll('.js-add-to-cart-button')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId
+
+      let productQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value) 
+
+      //Add to cart
+      addToCart(productId,productQuantity)
+      
+      //Interactive Cart Quantity
+      updateCartQuantity()
+      
+      //Disapperating added message after adding
+       addAddedElement(productId)
+      
+    })
+  })
+
+updateCartQuantity()
---
+++ scripts/checkout.js
@@ -0,0 +1,171 @@
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity} from "../data/cart.js";
+import { products } from "../data/products.js";
+import { fixCurrency } from "./utils/money.js";
+
+let checkoutCartHTML = ''
+
+cart.forEach((cartItem) => {
+  let matchingItem;
+  products.forEach((product) => {
+    if (cartItem.productId === product.id) {
+      matchingItem = product
+    }
+  })
+
+  checkoutCartHTML += `
+  <div class="cart-item-container js-cart-container-${matchingItem.id}">
+    <div class="delivery-date">
+      Delivery date: Tuesday, June 21
+    </div>
+
+    <div class="cart-item-details-grid">
+      <img class="product-image"
+        src="${matchingItem.image}">
+
+      <div class="cart-item-details">
+        <div class="product-name">
+          ${matchingItem.name}
+        </div>
+        <div class="product-price">
+          $${fixCurrency(matchingItem.priceCents)}
+        </div>
+        <div class="product-quantity">
+          <span>
+            Quantity: <span class="quantity-label js-quantity-label-${matchingItem.id}">${cartItem.quantity}
+            </span>
+          </span>
+          <span class="update-quantity-link link-primary js-update-quantity-link" data-product-id = ${matchingItem.id} >
+            Update
+          </span>
+          
+          <input class="quantity-input  js-quantity-input-${matchingItem.id}">
+          
+          <span class="save-quantity-link link-primary js-save-quantity-link"
+          data-product-id = ${matchingItem.id}>
+          Save
+          </span>
+          
+          <span class="delete-quantity-link link-primary js-delete-quantity-link"
+          data-product-id = ${matchingItem.id}>
+            Delete
+          </span>
+        </div>
+        <span class = "validation-text js-validation-text-${matchingItem.id}"></span>
+      </div>
+
+      <div class="delivery-options">
+        <div class="delivery-options-title">
+          Choose a delivery option:
+        </div>
+        <div class="delivery-option">
+          <input type="radio" checked
+            class="delivery-option-input"
+            name="delivery-option-${matchingItem.id}">
+          <div>
+            <div class="delivery-option-date">
+              Tuesday, June 21
+            </div>
+            <div class="delivery-option-price">
+              FREE Shipping
+            </div>
+          </div>
+        </div>
+        <div class="delivery-option">
+          <input type="radio"
+            class="delivery-option-input"
+            name="delivery-option-${matchingItem.id}">
+          <div>
+            <div class="delivery-option-date">
+              Wednesday, June 15
+            </div>
+            <div class="delivery-option-price">
+              $4.99 - Shipping
+            </div>
+          </div>
+        </div>
+        <div class="delivery-option">
+          <input type="radio"
+            class="delivery-option-input"
+            name="delivery-option-${matchingItem.id}">
+          <div>
+            <div class="delivery-option-date">
+              Monday, June 13
+            </div>
+            <div class="delivery-option-price">
+              $9.99 - Shipping
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+`
+})
+
+document.querySelector('.js-order-summary')
+  .innerHTML = checkoutCartHTML
+
+document.querySelectorAll('.js-delete-quantity-link')
+  .forEach((deleteLink) => {
+    deleteLink.addEventListener('click', () => {
+      const productId = deleteLink.dataset.productId
+        removeFromCart(productId)
+        
+        const container = document.querySelector(`.js-cart-container-${productId}`)
+        container.remove()
+        updateCartQuantity()
+    })
+  })
+
+function updateCartQuantity() {
+  document.querySelector('.js-return-to-home-link').innerHTML = `${calculateCartQuantity()} items`
+}
+
+
+document.querySelectorAll('.js-update-quantity-link')
+  .forEach((updateLink) => {
+    updateLink.addEventListener('click', () => {
+      const productId = updateLink.dataset.productId
+      console.log(productId)
+      document.querySelector(`.js-cart-container-${productId}`).classList.add('is-editing-quantity')
+
+      let inputBoxQuantity;
+      cart.forEach((cartItem) => {
+        if (cartItem.productId === productId) {
+          inputBoxQuantity = cartItem.quantity
+        }
+      })
+      const quantityInputElement = document.querySelector(`.js-quantity-input-${productId}`) 
+      quantityInputElement.value = inputBoxQuantity
+
+      quantityInputElement.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+          saveNewQuantity(updateLink)
+        }
+      })
+    })
+  })
+
+document.querySelectorAll('.js-save-quantity-link')
+  .forEach((saveLink) => {
+    saveLink.addEventListener('click', () => {
+      saveNewQuantity(saveLink)
+    })
+  })
+  
+function saveNewQuantity(saveLink) {
+  const productId = saveLink.dataset.productId
+  document.querySelector(`.js-cart-container-${productId}`).classList.remove('is-editing-quantity')
+  const newQuantity = Number(document.querySelector(`.js-quantity-input-${productId}`).value)
+  
+  if (newQuantity>=0 && newQuantity < 1000) {
+    document.querySelector(`.js-validation-text-${productId}`).innerHTML = ""
+    updateQuantity(productId, newQuantity)
+    document.querySelector(`.js-quantity-label-${productId}`).innerHTML = newQuantity
+    updateCartQuantity()
+  } else {
+    document.querySelector(`.js-validation-text-${productId}`).innerHTML = "Enter a valid quantity"
+  }
+}
+
+  updateCartQuantity()
